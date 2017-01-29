e1bac2d25ff003ecdad7ca41775433406e916e4c
Frederico Lohmann Jr <flohmannjr@yahoo.com>
2017-01-29 11:51:21 -0200

    Done.

--- MoshCSharpBasics09/Program.cs
+++ MoshCSharpBasics09/Program.cs
@@ -1,8 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+using System.IO;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace MoshCSharpBasics09
 {
@@ -10,6 +8,30 @@ namespace MoshCSharpBasics09
     {
         static void Main(string[] args)
         {
+            //var path = @"c:\users\fred\documents\visual studio 2015\Projects\MoshCSharpBasics09\MoshCSharpBasics09\";
+            //var exercise = "Program.cs";
+            var path = @"c:\users\fred\documents\";
+            var exercise = "TombRaider.log";
+            var fullPath = path + exercise;
+
+            var exerciseContent = File.ReadAllText(fullPath);
+            var words = exerciseContent.Split(' ');
+            var longestWord = "";
+
+            foreach (var word in words)
+                if (word.Length > longestWord.Length)
+                    longestWord = word;
+
+            var output = new StringBuilder();
+
+            output
+                .Append("File: " + fullPath)
+                .AppendLine()
+                .Append("Word count: " + words.Length)
+                .AppendLine()
+                .Append("Longest word: " + longestWord);
+
+            Console.WriteLine(output);
         }
     }
 }
